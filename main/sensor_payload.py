0c665b8337a5700d152f66b7a9d14250137c75a4
mouryarahul <mourya.rahul1981@gmail.com>
2021-01-12 23:35:55 +0000

    minor changes

--- main/sensor_payload.py
+++ main/sensor_payload.py
@@ -38,47 +38,190 @@ class SensorPayload:
 
     def __init__(self):
         """Initialise."""
-        pass
+        super().__init__()
 
     def __call__(self):
         return self
 
     def get_est_acquisition_duration(self) -> float:
         """Get estimated acquisition duration in seconds."""
-        return 0.0
+        pass
 
     def start_acquisition(self):
         """Start an acquisition. Returns True if started successfully."""
-        return True
+        pass
 
     def process_acquisition(self):
         """Continue processing the acquisition. To be called periodically within the mainloop. 
         This is where the state machine keeps track on progress. """
-        return None
+        pass
 
     def is_completed(self):
         """Is acquisition completed flag."""
-        return True
+        pass
 
     def get_latest_data_as_bytes(self) -> bytes:
         """Get the latest data as a bytes."""
         # Format to be determined
-        return None
+        pass
 
     def get_latest_data_as_json(self):
         """Get the latest data as a json object. Which can then be loaded into json.dump/dumps."""
-        return None
+        pass
 
+import pyb
+from machine import I2C
+import struct
 
 import pybd_expansion.main.bme280 as bme280
 from pybd_expansion.main.bme280 import BME280
 
 import pybd_expansion.main.lsm303agr as lsm303agr
 from pybd_expansion.main.lsm303agr import LSM303AGR
-import pyb
 
-import struct
+from pybd_expansion.main.ms5837 import MS5837
+from pybd_expansion.main.tsys01 import TSYS01
 
+
+class ExternalSensorPayload(SensorPayload):
+    """ Provides Pressure, Depth and Temperature readings
+     from BlueRobotics Underwater Pressure Sensor MS5837-30BA. """
+
+    def __init__(self):
+        """ Initialise """
+        SensorPayload.__init__(self)
+
+        self._ldo2 = None
+        self._scl = None
+        self._sda = None
+        self._i2c = None
+        self._ms5837 = None
+        self._tsys01 = None
+        self._acquisition_duration = None
+        self._ms5837_awaiting_valid_measurements = False
+        self._tsys01_awaiting_valid_measurements = False
+        self._ms5837_pressure = None
+        self._ms5837_temperature = None
+        self._ms5837_depth = None
+        self._tsys01_temperature = None
+
+    def __call__(self):
+        return self
+
+    def get_est_acquisition_duration(self) -> float:
+        """Get estimated acquisition duration in seconds."""
+        return self._acquisition_duration
+
+    def stop_acquisition(self) -> None:
+        """ Stop an acquisition: disable power to sensor,
+        clear i2c bus and other objects to None"""
+        self._ldo2.value(0)  # disable ldo2
+        self._ldo2 = None
+        self._scl.value(0)  # disable pull-up
+        self._scl = None
+        self._sda.value(0)  # disable pull-up
+        self._sda = None
+        self._i2c = None
+        self._ms5837 = None
+        self._tsys01 = None
+        self._ms5837_pressure = None
+        self._ms5837_temperature = None
+        self._ms5837_depth = None
+        self._tsys01_temperature = None
+        self._acquisition_duration = None
+        self._ms5837_awaiting_valid_measurements = False
+        self._tsys01_awaiting_valid_measurements = False
+
+    def start_acquisition(self):
+        """Start an acquisition. Returns True if started successfully."""
+        # Ensure the sensors are powered and the I2C pullups are enabled.
+        self._ldo2 = pyb.Pin('Y5', mode=pyb.Pin.OPEN_DRAIN, pull=None, value=1)
+        self._scl = pyb.Pin('PULL_SCL', mode=pyb.Pin.OUT, value=1)  # enable 5.6kOhm X9/SCL pull-up
+        self._sda = pyb.Pin('PULL_SDA', mode=pyb.Pin.OUT, value=1)  # enable 5.6kOhm X10/SDA pull-up
+
+        # Instantiate and Initialize the i2c-bus X
+        self._i2c = I2C('X', freq=400000)
+
+        pyb.delay(10)  # in ms
+
+        # Instantiate and Initialize the Pressure Sensor
+        self._ms5837 = MS5837(self._i2c)
+        self._tsys01 = TSYS01(self._i2c)
+
+        init_success_1 = self._ms5837.init()
+        pyb.delay(10)  # in ms
+        init_success_2 = self._tsys01.init()
+
+        if init_success_1 and init_success_2:
+            # Clear measurements
+            self._ms5837_pressure = None
+            self._ms5837_temperature = None
+            self._ms5837_depth = None
+            self._tsys01_temperature = None
+            self._ms5837_awaiting_valid_measurements = True
+            self._tsys01_awaiting_valid_measurements = True
+            return True
+        else:
+            return False
+
+    def process_acquisition(self):
+        """Continue processing the acquisition. To be called periodically within the mainloop.
+            This is where the state machine keeps track on progress. """
+        if self._ms5837 and self._tsys01:
+            acquisition_duration = 0
+            if self._ms5837_awaiting_valid_measurements:
+                start_micro = pyb.micros()
+                read_success = self._ms5837.read()
+                if read_success:
+                    self._ms5837_pressure = self._ms5837.pressure()
+                    self._ms5837_temperature = self._ms5837.temperature()
+                    self._ms5837_awaiting_valid_measurements = False
+
+                acquisition_duration += pyb.elapsed_micros(start_micro)
+
+            pyb.delay(10)  # in ms
+            if self._tsys01_awaiting_valid_measurements:
+                start_micro = pyb.micros()
+                read_success = self._tsys01.read()
+                if read_success:
+                    self._tsys01_temperature = self._tsys01.temperature()
+                    self._tsys01_awaiting_valid_measurements = False
+
+                acquisition_duration += pyb.elapsed_micros(start_micro)
+
+            self._acquisition_duration = acquisition_duration
+
+        else:
+            raise Exception("Sensors are not initialized!")
+
+    def is_completed(self) -> bool:
+        """Is acquisition completed flag."""
+        return ((not self._ms5837_awaiting_valid_measurements)
+                and (not self._tsys01_awaiting_valid_measurements))
+
+    def get_latest_pressure_in_mbar(self) -> float:
+        """ Get the latest pressure measurement in MBar """
+        return self._ms5837_pressure
+
+    def get_latest_temperature_in_celsius(self) -> float:
+        """ Get the latest temperature measurement in degree Celsius """
+        return self._tsys01_temperature
+
+    def get_latest_depth_as_float(self, fluid_density: float = 1000.0):
+        """ Get the Depth (calculated using pressure and fluid_density in kg/m^3) """
+        self._ms5837.set_fluid_density(fluid_density)
+        return self._ms5837.depth()
+
+    def get_latest_data_as_bytes(self) -> bytes:
+        """Get the latest data as a bytes."""
+        # Format as packed floats
+        # https://docs.python.org/3/library/struct.html
+        packed_bytes = struct.pack("ff", self._ms5837_pressure, self._tsys01_temperature)
+
+        # To unpack
+        # ms5837_pressure, ms5837_temperature = struct.unpack("ff", packed_bytes)
+
+        return packed_bytes
 
 
 class PebSensorPayload(SensorPayload):
@@ -103,9 +246,6 @@ class PebSensorPayload(SensorPayload):
         self._lsm303agr_temperature = None
         self._lsm303agr_accel = None
         self._lsm303agr_magneto = None
-
-
-        pass
 
     def __call__(self):
         return self
@@ -167,12 +307,11 @@ class PebSensorPayload(SensorPayload):
 
         self._lsm303agr_awaiting_valid_measurements = True
 
-
         return True
 
     def process_acquisition(self):
         """Continue processing the acquisition. To be called periodically within the mainloop.
-        This is where the state machine keeps track on progress. """
+            This is where the state machine keeps track on progress. """
 
         if self._bme280_awaiting_valid_measurements:
             reg_val, im_update, measuring = self._bme280.get_status_reg()
@@ -216,7 +355,6 @@ class PebSensorPayload(SensorPayload):
                 self._lsm303agr.set_magneto_cfg_reg_a(magneto_odr=lsm303agr.MAGNETO_ODR_10HZ,
                                                       magneto_md=lsm303agr.MAGNETO_MD_IDLE0, lp=0, comp_temp_en=1)
 
-
         return None
 
     def is_completed(self):
@@ -255,7 +393,12 @@ class PebSensorPayload(SensorPayload):
         return jason
 
 
-def get_sensor_payload_instance() -> SensorPayload:
+def get_internal_sensor_payload_instance() -> SensorPayload:
     """Get instance of the SensorPayload for this module.
     Override this function in derived modules."""
     return PebSensorPayload()
+
+def get_external_sensor_payload_instance() -> SensorPayload:
+    """Get instance of the SensorPayload for this module.
+    Override this function in derived modules."""
+    return ExternalSensorPayload()
